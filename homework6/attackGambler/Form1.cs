f97441dc9f242042857972b19c1bd6bace5d4858
Enri Fort <enrifort99@gmail.com>
2023-11-19 16:47:28 +0100

    .

---
+++ homework6/attackGambler/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace attackGambler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawLine(Graphics g, int nAttacks, int S, int [] frequencies, Rectangle rect)
+        {
+            const int rangeScaler = 4;
+
+            double minValue = -rangeScaler * Math.Sqrt(nAttacks);
+            double maxValue = rangeScaler * Math.Sqrt(nAttacks);
+
+            double rangeValues = maxValue - minValue;
+
+            int y = 0; // Y coordinates
+            int P; // penetration Score
+
+
+            // The line start from the origin
+            List<Point> myLine = new List<Point> { new Point(rect.Left, rect.Top + rect.Height) };
+
+            for (int x = 1; x <= nAttacks; x++)
+            {
+
+                y += Utils.RandomUtils.RademacherDist(0.5); // generate the random value for the attack
+
+                int xDevice = Utils.Drawing.XtoDevice((double)x / nAttacks, 0, 1, rect.Left, rect.Width);
+                int yDevice = Utils.Drawing.YtoDevice(y, minValue, rangeValues, rect.Top, rect.Height);
+
+                myLine.Add(new Point(xDevice, yDevice));
+
+                // P penetration score (upper line)
+                // S security score (lower line)
+
+                if (y == S) // If the system reaches a security score of S is OKAY
+                {
+                    continue;
+                }
+                else
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        P = (k + 1) * 10; // Compute value of P bounder
+                        if (y == P) // If the system reaches the penetration score P
+                        {
+                            frequencies[k] += 1; // number of frequencies which fall in intervall k
+                        }
+                    }
+                }
+
+            }
+
+            Color lineColor = Utils.RandomUtils.GetRandomColor(); // Get a random color 
+            Pen pen = new Pen(lineColor, 1) { DashStyle = DashStyle.Solid };
+            Point[] lineArray = myLine.ToArray();
+
+            g.DrawLines(pen, lineArray);
+        }
+
+        // Main
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nSystems = int.Parse(textBox1.Text);
+            int nAttacks = int.Parse(textBox2.Text);
+            int securityScore = -int.Parse(textBox3.Text);
+
+            pictureBox1.Image = null;
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            Rectangle rectChart = new Rectangle(20, 30, pictureBox1.Width - 250, pictureBox1.Height - 60); // rectangle for the lines
+            int[] penetrations = new int[10]; // contains the FREQUENCIES of the penetrations
+
+            for (int m = 1; m <= nSystems; m++)
+            {
+                drawLine(g, nAttacks, securityScore, penetrations, rectChart);
+            }
+
+            g.DrawRectangle(Pens.WhiteSmoke, rectChart);
+            Rectangle rectHisto = new Rectangle(Utils.Drawing.XtoDevice(nAttacks, 0, nAttacks, rectChart.X, rectChart.Width), rectChart.Y, 210, rectChart.Height);  // rectangle for the histograms 
+            g.DrawRectangle(new Pen(Color.LightBlue) { DashStyle = DashStyle.Dash }, rectHisto); // dotted line
+
+            Utils.Drawing.DrawHistogramsFromArr(g, penetrations, rectHisto, Color.Red, Color.LightGreen);
+            pictureBox1.Image = bmp;
+        }
+    }
+}
---
+++ homework6/attackGambler/Utils.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+public static class Utils
+{
+
+    public static class RandomUtils
+    {
+        private static Random random = new Random();
+
+        public static int BernoulliDist(double p)
+        {
+            return (random.NextDouble() <= p) ? 1 : 0;
+        }
+
+        public static int RademacherDist(double p)
+        {
+            return (random.NextDouble() <= p) ? 1 : -1;
+        }
+
+        // To generate a random color
+        public static Color GetRandomColor()
+        {
+            HashSet<Color> usedColors = new HashSet<Color>();
+            while (true)
+            {
+                int R = random.Next(256);
+                int G = random.Next(256);
+                int B = random.Next(256);
+
+                Color randomColor = Color.FromArgb(R, G, B);
+
+                if (!usedColors.Contains(randomColor))
+                {
+                    usedColors.Add(randomColor);
+                    return randomColor;
+                }
+            }
+        }
+
+    }
+
+
+    public static class Drawing
+    {
+
+        public static int XtoDevice(double X, double minX, int rangeX, double left, double width)
+        {
+            return (int)(left + width * (X - minX) / rangeX);
+        }
+
+        public static int YtoDevice(int Y, double minY, double rangeY, double top, double height)
+        {
+            return (int)(top + height - (height * (Y - minY) / rangeY));
+        }
+
+        public static void DrawHistogramsFromArr(Graphics g, int[] frequencies, Rectangle devRect, Color startGrdColor, Color endGrdColor)
+        {
+            int bins = frequencies.Length;
+            int maxFrequency = 0;
+            int gap = 1;
+            double barHeightFactor = 0.4; // Adjust this factor to control bar height
+
+            // Get the maximum value
+            for (int i = 0; i < bins; i++)
+            {
+                maxFrequency = Math.Max(maxFrequency, frequencies[i]);
+            }
+
+            for (int i = 0; i < bins; i++)
+            {
+                // Width and height are reversed because the histogram is horizontal
+                int barWidth = (int)(frequencies[i] / (double)maxFrequency * devRect.Width - 1);
+                int barHeight = (int)((devRect.Height - (bins - 1) * gap) / (double)bins * barHeightFactor);
+
+
+                // Ensure the bar width is at least 1
+                barWidth = Math.Max(barWidth, 1);
+
+                int x = devRect.X + 2;
+                int y = devRect.Top + (devRect.Height / 4) + (i + 1) * (barHeight + gap);
+
+                Rectangle rectHisto = new Rectangle(x, y, barWidth, barHeight);
+
+                // Draw rectangle
+                g.DrawRectangle(Pens.Black, rectHisto);
+
+                // Create gradient
+                LinearGradientBrush gradientBrush = new LinearGradientBrush(rectHisto, startGrdColor, endGrdColor, LinearGradientMode.Horizontal);
+
+                // Fill with gradient
+                g.FillRectangle(gradientBrush, rectHisto);
+
+                // Add legend
+                Font font = new Font("Verdana", 9);
+                Brush brush = new SolidBrush(Color.White);
+                g.DrawString(frequencies[i].ToString(), font, brush, rectHisto.X, rectHisto.Y + (rectHisto.Height / 2));
+            }
+
+        }
+
+    }
+}
